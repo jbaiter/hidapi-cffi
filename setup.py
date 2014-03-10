aceb34e3703cfe7f3c6ea0008924f3eacb61ad2d
Johannes Baiter <johannes.baiter@gmail.com>
2014-03-10 23:46:13 +0100

    Add license and setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='hidapi-cffi',
+    version="0.1",
+    description=("CFFI wrapper for hidapi"),
+    author="Johannes Baiter",
+    url="http://github.com/jbaiter/hidapi-cffi.git",
+    author_email="johannes.baiter@gmail.com",
+    license='BSD',
+    py_modules=['hidapi'],
+    setup_requires=['cffi >= 0.8']
+)
